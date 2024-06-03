2d398a6a8752c6aa1edb2f922bd1eb978bb623c8
ubaid7470 <obaidkhan2667@gmail.com>
2024-06-03 14:50:15 +0500

    Pages and Routs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
-  return <div className="text-orange-500">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
