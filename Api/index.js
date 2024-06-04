ffa4455429fec7b61440a6cf99c760a959d97fc2
ubaid7470 <obaidkhan2667@gmail.com>
2024-06-05 00:31:35 +0500

    created middleware and errorHandler function

--- Api/index.js
+++ Api/index.js
@@ -25,3 +25,14 @@ app.listen(3000, () => {
 
 app.use("/api/user", UserRoute);
 app.use("/api/auth", AuthRoute);
+
+//middle ware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
